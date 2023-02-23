bc4e99298b7a1763f05b954498665835def24cff
Pierce Langdon <langdon.jp@gmail.com>
2023-02-23 12:25:21 +1100

    Create reset grid button, add header text

--- js/script.js
+++ js/script.js
@@ -2,19 +2,25 @@ const sketchContainer = document.querySelector(".sketch-container");
 const gridSizeButton = document.querySelector(".grid-size-button");
 const pixels = 16;
 
+// Draw initial grid
 drawGrid(pixels);
 
-gridSizeButton?.addEventListener("click", drawGrid);
+gridSizeButton?.addEventListener("click", () => {
+  drawGrid(getSize());
+});
 
 // Helper functions
 
 function drawGrid(pixelAmt) {
+  const gridWrapper = document.createElement("div");
+  gridWrapper.classList.add("grid-wrapper");
+
   for (let i = 0; i < pixelAmt; i++) {
     const row = document.createElement("div");
     row.classList.add("pixel-row");
 
     for (let j = 0; j < pixelAmt; j++) {
-      const widthHeight = 900 / pixels;
+      const widthHeight = 700 / pixelAmt;
       const pixel = document.createElement("div");
       pixel.classList.add("pixel");
       pixel.style.height = `${widthHeight}px`;
@@ -26,15 +32,24 @@ function drawGrid(pixelAmt) {
       row.appendChild(pixel);
     }
 
-    sketchContainer?.appendChild(row);
+    gridWrapper?.appendChild(row);
   }
+  sketchContainer?.appendChild(gridWrapper);
 }
 
 function getSize() {
-  const size = prompt(
-    'How large do you want the grid to be in pixels? (E.g., entering "50" will create a 50x50 pixel grid.'
+  let size = Number(
+    prompt(
+      'How large do you want the grid to be in pixels? (E.g., entering "50" will create a 50x50 pixel grid.'
+    )
   );
-  return Number(size);
+
+  while (size > 100 || size < 10) {
+    size = Number(prompt("Please pick a number between 10 and 100."));
+  }
+  const gridWrapper = document.querySelector(".grid-wrapper");
+  gridWrapper?.remove();
+  return size;
 }
 
 function getColor() {
