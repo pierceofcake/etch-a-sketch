15d72511f1639e7590ed8ead6b1025c7dc3ff77a
Pierce Langdon <langdon.jp@gmail.com>
2023-02-23 11:41:49 +1100

    Recreate grid drawing function logic and CSS CSS grid layout was not working as expected. Switched to flexbox layout and changed logic for drawing the grid to a nested loop. Fixed issue where pixel would only change color on hover but changed back to white after leaving the div.

--- js/script.js
+++ js/script.js
@@ -1,16 +1,41 @@
 const sketchContainer = document.querySelector(".sketch-container");
+const gridSizeButton = document.querySelector(".grid-size-button");
+const pixels = 16;
 
-for (let i = 0; i < 16; i++) {
-  for (let j = 0; j < 16; j++) {
-    const pixel = document.createElement("div");
-    pixel.classList.add("pixel");
-    sketchContainer?.appendChild(pixel);
+drawGrid(pixels);
+
+gridSizeButton?.addEventListener("click", drawGrid);
+
+// Helper functions
+
+function drawGrid(pixelAmt) {
+  for (let i = 0; i < pixelAmt; i++) {
+    const row = document.createElement("div");
+    row.classList.add("pixel-row");
+
+    for (let j = 0; j < pixelAmt; j++) {
+      const widthHeight = 900 / pixels;
+      const pixel = document.createElement("div");
+      pixel.classList.add("pixel");
+      pixel.style.height = `${widthHeight}px`;
+      pixel.style.width = `${widthHeight}px`;
+
+      pixel.addEventListener("mouseenter", () => {
+        pixel.style.backgroundColor = getColor();
+      });
+      row.appendChild(pixel);
+    }
+
+    sketchContainer?.appendChild(row);
   }
 }
 
-document.documentElement;
-
-// Helper functions
+function getSize() {
+  const size = prompt(
+    'How large do you want the grid to be in pixels? (E.g., entering "50" will create a 50x50 pixel grid.'
+  );
+  return Number(size);
+}
 
 function getColor() {
   let letters = "0123456789ABCDEF";
